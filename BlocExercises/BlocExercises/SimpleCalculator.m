8676daa60a8e86e7d94180611bff10f53a1d7f03
TREVOR AHLERT <trevor.ahlert@gmail.com>
2014-07-11 07:49:47 -0400

    Completed numbers and variables

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -12,17 +12,21 @@
 
 - (NSInteger) increaseNumberBy1:(NSInteger) number {
     /* WORK HERE */
+    number++;
     return number;
 }
 
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
     /* WORK HERE */
-    return number1;
+    NSInteger additionResult = number1 + number2;
+    return additionResult;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
     /* WORK HERE */
-    return 0;
+    NSInteger remainderResult = dividend % divisor;
+    return remainderResult;
+    
 }
 
 @end
