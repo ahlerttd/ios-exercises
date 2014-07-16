26941008c4256a75566268d6390652f01421a5ba
TREVOR AHLERT <trevor.ahlert@gmail.com>
2014-07-16 14:00:17 -0400

    Completed loops

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,42 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    NSMutableString *updatedCount = [@"" mutableCopy];
+    NSInteger count = number;
+    NSInteger countOther = otherNumber;
+    
+    if (number < otherNumber){
+    
+    
+    while (number <= otherNumber) {
+        [updatedCount appendString: [NSString stringWithFormat:@"%ld", (long)count]];
+        count = ++number;
+        
+    }
+    }
+    
+        else if (otherNumber <= number) {
+            while (otherNumber <= number) {
+                [updatedCount appendString: [NSString stringWithFormat:@"%ld", (long)countOther]];
+                countOther = ++otherNumber;
+        }
+            
+    
+    }
+    
+        else if (otherNumber == number) {
+           
+                [updatedCount appendString: [NSString stringWithFormat:@"%ld", (long)number]];
+         
+            
+            
+        }
+    
+    
+    
+    
+
+   return updatedCount;
 }
 
 @end
